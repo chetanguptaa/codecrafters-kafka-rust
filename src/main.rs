b2f460faf9e93ae8dbb52d1e7aeb619a3bbbd737
Chetan Gupta <chetanguptaaaa21@gmail.com>
2026-01-01 23:34:08 +0530

    send correlation id

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,50 @@
 #![allow(unused_imports)]
+use std::io::Write;
 use std::net::TcpListener;
 
+struct HeaderV0 {
+    correlation_id: u32,
+}
+
+struct Message {
+    message_size: u32,
+    header: HeaderV0,
+}
+
+impl Message {
+    fn new(message_size: u32, correlation_id: u32) -> Message {
+        Message {
+            message_size,
+            header: HeaderV0 { correlation_id },
+        }
+    }
+}
+
 fn main() {
-    println!("Logs from your program will appear here!");
+    let message = Message::new(2, 7);
+    let message_id = message
+        .message_size
+        .to_be_bytes()
+        .iter()
+        .map(|b| format!("{:02X}", b))
+        .collect::<Vec<_>>()
+        .join(" ");
+    let correlation_id = message
+        .header
+        .correlation_id
+        .to_be_bytes()
+        .iter()
+        .map(|b| format!("{:02X}", b))
+        .collect::<Vec<_>>()
+        .join(" ");
+
     let listener = TcpListener::bind("127.0.0.1:9092").unwrap();
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
+            Ok(mut stream) => {
+                use std::io::Write;
+                let response = format!("{message_id}\n{correlation_id}\n");
+                stream.write_all(response.as_bytes()).unwrap();
             }
             Err(e) => {
                 println!("error: {}", e);
